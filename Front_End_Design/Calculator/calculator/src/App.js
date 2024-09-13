23e7ecad1c0663ceb4016a1c7e25c11b990a4ee3
kithenry <21903316@emu.edu.tr>
2024-09-13 17:36:35 +0200

    added basic calculation features

--- Front_End_Design/Calculator/calculator/src/App.js
+++ Front_End_Design/Calculator/calculator/src/App.js
@@ -1,8 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-
-
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
 /* #### User story One
  * - User clicks buttons denoting expression for which they need calculation
@@ -14,161 +13,230 @@ import React from 'react';
  *        and add it to some storage buffer then continue popping items from th e list.
  *      - if anything that is not an operand or a number is encountered, return error
  *      - if another number is encountered, append it to the buffer after parsing it to digit data type.
- *      - if an operator is encountered (one of + / - *), store it in a seperate operator buffer. 
+ *      - if an operator is encountered (one of + / - *), store it in a seperate operator buffer.
  *      - if anything  other than a number or space is encountered on the next pop after detecting an operator, return error.
  *      - If a number is encountered, parse it to string. Now send the expression over to the calculator which will return a result based off of the operand,
  *      - every expression while parsing will therefore have 2 operands and one operator each.
  *      - there shall be two operant buffers and one operator buffer.
- *      - there shall be one result buffer, this shall by default hold the value 0. 
+ *      - there shall be one result buffer, this shall by default hold the value 0.
  *      - the result buffer will after every evaluation be equal to the result of that evaluation.
  *
  * - User gets the result printed to the screen
  * */
 
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentInputValue: "",
+      currentOutputValue: "",
+      previousInput: "", // for formatting purposes
+      lastResult: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.arithmeticHandler = this.arithmeticHandler.bind(this);
+    this.parser = this.parser.bind(this);
+    this.isOperand = this.isOperand.bind(this);
+    this.isOperator = this.isOperator.bind(this);
+    this.operands = "1 2 3 4 5 6 7 8 9 0".split(" ");
+    this.operators = "+ * / -".split(" ");
+  }
 
+  isOperand(token) {
+    if (token !== null) {
+      return this.operands.includes(token.toString());
+    } else {
+      return false;
+    }
+  }
 
+  isOperator(token) {
+    if (token !== null) {
+      return this.operators.includes(token.toString());
+    }
+    return false;
+  }
 
+  parser(expressionString) {
+    // so that popping seems to be getting items first to last (left to right)
+    expressionString = expressionString.split(" ").reverse();
+    expressionString = expressionString.filter((item) => item.trim() !== "");
+    console.log("Entered parser mode");
+    let currentToken;
+    let previousToken = this.state.lastResult;
+    let result = null;
+    let operandOne = null;
+    let operandTwo = null;
+    let operator = null;
+    let expressionFull = false;
+    if (expressionString.length <= 1) {
+      result = this.arithmeticHandler(operandOne, operator, operandTwo);
+      console.log(result);
+      return;
+    }
+    let i = 0;
+    while (expressionString.length > 0) {
+      currentToken = expressionString.pop();
+      i++;
+      console.log(
+        "popped for the " +
+          i +
+          "th time. | Expression now: " +
+          expressionString +
+          " | Pop Value: " +
+          currentToken,
+      );
+      if (this.isOperand(currentToken)) {
+        if (previousToken === null) {
+          // first token
+          operandOne = parseInt(currentToken);
+          previousToken = currentToken;
+        } else if (this.isOperator(previousToken)) {
+          // 4+5, 6+3, +4, -8, /9 scenario
+          // we have a full expression
+          operandTwo = parseInt(currentToken);
+          result = this.arithmeticHandler(operandOne, operator, operandTwo);
+          previousToken = toString(result);
+        }
+      } else if (this.isOperator(currentToken)) {
+        if (this.isOperand(previousToken)) {
+          // 2+, 2-, 2*, 4/ scenario
+          operator = currentToken;
+          previousToken = currentToken;
+        } else if (previousToken === null) {
+          operator = currentToken;
+          // error
+        }
+      }
+    }
+    this.setState({ lastResult: result, currentOutputValue: result });
+  }
 
+  arithmeticHandler(oper1, action, oper2) {
+    if (oper1 === null && this.state.lastResult === null) {
+      switch (action) {
+        case "+":
+          return oper2;
+        case "-":
+          return -oper2;
+      }
+    } else if (oper1 === null && this.state.lastResult !== null) {
+      oper2 = this.state.lastResult;
+    }
+    if (oper1 === null && oper2 === null && action === null) {
+      return this.state.lastResult;
+    }
+    switch (action) {
+      case "*":
+        return oper1 * oper2;
+      case "+":
+        return oper1 + oper2;
+      case "-":
+        return oper1 - oper2;
+      case "/":
+        return oper1 / oper2;
+      default:
+        return;
+    }
+  }
 
-class Calculator extends React.Component{
-	constructor(props){
-		super(props);
-		this.state  = {
-			currentInputValue: '',
-			curretOutputValue: ''
-		}
-		this.handleClick = this.handleClick.bind(this);
-		this.arithmeticHandler = this.arithmeticHandler.bind(this);
-		this.parser = this.parser.bind(this);
-		this.isOperand = this.isOperand.bind(this);
-		this.isOperator = this.isOperator.bind(this);
-		this.operands = '1 2 3 4 5 6 7 8 9 0'.split(' ');
-		this.operators = '+ * / -'.split(' ');
-	}
+  handleClick(event) {
+    event.preventDefault();
+    // update inputState
+    let targetValue = event.target.value;
+    this.setState({ previousInput: targetValue });
+    let validOps = "CLR = + - * / 1 2 3 4 5 6 7 8 9 0".split(" ");
 
-	isOperand(token){
-		return operands.includes(token.toString());
-	}
+    if (validOps.includes(targetValue)) {
+      if (
+        this.isOperand(this.state.previousInput) &&
+        this.isOperand(targetValue)
+      ) {
+        console.log("here");
+        this.setState((state) => ({
+          currentInputValue: state.currentInputValue + targetValue,
+        }));
+      } else if (targetValue !== "CLR" && targetValue !== "=") {
+        this.setState((state) => ({
+          currentInputValue: state.currentInputValue + " " + targetValue,
+        }));
+      } else if (targetValue === "CLR") {
+        this.setState({
+          currentInputValue: "",
+          currentOutputValue: "",
+        });
+      } else if (targetValue === "=") {
+        // modification needed
+        let expressionString = this.state.currentInputValue;
+        this.parser(expressionString);
+      } else {
+        this.setState((state) => ({
+          currentInputValue: state.currentInputValue + " " + targetValue,
+        }));
+      }
+      // wait for enter button to validate input
+    }
+  }
 
-	isOperator(token){
-		return operators.includes(token.toString());
-	}
-
-	parser(){
-
-		console.log('Entered parser mode');
-		let operandOne;
-		let operandTwo;
-		let operator;
-		let result;
-		let expression = this.state.currentInputValue.split(' ').reverse();
-		let currentToken;
-		let previousToken = '';
-		let tempExpression = '';
-		let success = '';
-		while(expression.length > 0){
-			currentToken =  expression.pop();
-			if (this.isOperand(currentToken)){
-				if(this.isOperand(previousToken)){
-					tempExpression += currentToken
-					previousToken = currentToken
-					continue;
-				} else if (this.isOperator(previousToken)){
-					tempExpression += currentToken;
-					result = this.evaluateExpression(expression);
-					continue;
-				} else {
-					tempExpression = currentToken;
-					previousToken = currentToken;
-					continue;
-				}
-			} else if (this.isOperator(currentToken)){
-				if(this.isOperator(previousToken)){
-					console.log('error: Bad expression');
-					success = false;
-					break;
-				} else if (this.isOperand(previousToken)) {
-					tempExpression += currrentToken;
-					previousToken = currentToken;
-					continue;
-				} else {
-					// user entered operator first without any operands
-					success = false;
-					break;
-				}
-
-			}
-		}
-
-	}
-
-	arithmeticHandler(){
-
-
-	}
-
-        handleClick(event){
-		event.preventDefault();
-		// update inputState
-		let targetValue = event.target.value;
-		let validOps = '+ - * / 1 2 3 4 5 6 7 8 9 0'.split(' ');
-		if(validOps.includes(targetValue)){
-			this.setState((state)=>(
-			{currentInputValue: state.currentInputValue + ' ' + event.target.value}
-		))
-		} else if (targetValue === 'CLR'){
-			this.setState(
-				{
-					currentInputValue:'',
-				}
-			)
-		} else if (targetValue === '='){
-			this.parser();
-		}
-		// wait for enter button to validate input
-
-	}
-
-
-
-	render(){
-		// ui setup
-		let operands = [1,2,3,4,5,6,7,8,9,0];
-		let operators = '+ - / CLR * ='.split(' ');
-		return(
-			<div>
-	                   <p>Operands:</p>
-			   {operands.map(operand=>(<button onClick={this.handleClick} className='button btn-secondary' value={operand} key={operand}>{operand}</button>))}
-			   <p>Operators:</p>
-			   {operators.map(operator=>(<button onClick={this.handleClick} className='button btn-secondary' value={operator} key={operator} >{operator}</button>))}
-			<hr />
-			<p> This is the output panel </p>
-			<input value={this.state.currentInputValue} className='currentInput' disabled />
-			<input value={this.state.currentOutputValue} className='currentOutput' disabled />
-			</div>
-		)
-	}
-
+  render() {
+    // ui setup
+    let operands = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    let operators = "+ - / CLR * =".split(" ");
+    return (
+      <div>
+        <p>Operands:</p>
+        {operands.map((operand) => (
+          <button
+            onClick={this.handleClick}
+            className="button btn-secondary"
+            value={operand}
+            key={operand}
+          >
+            {operand}
+          </button>
+        ))}
+        <p>Operators:</p>
+        {operators.map((operator) => (
+          <button
+            onClick={this.handleClick}
+            className="button btn-secondary"
+            value={operator}
+            key={operator}
+          >
+            {operator}
+          </button>
+        ))}
+        <hr />
+        <p> This is the output panel </p>
+        <input
+          value={this.state.currentInputValue}
+          className="currentInput"
+          disabled
+        />
+        <input
+          value={this.state.currentOutputValue}
+          className="currentOutput"
+          disabled
+        />
+      </div>
+    );
+  }
 }
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-
-class App extends React.Component{
-	constructor(props){
-		super(props);
-	}
-
-	render(){
-		return(
-			<div>
-			 This is a test line.
-			<hr />
-			<Calculator />
-			</div>
-
-		)
-	}
+  render() {
+    return (
+      <div>
+        This is a test line.
+        <hr />
+        <Calculator />
+      </div>
+    );
+  }
 }
-
 
 export default App;
--- Front_End_Design/MarkDown_Previewer/md_previewer/src/App.js
+++ Front_End_Design/MarkDown_Previewer/md_previewer/src/App.js
@@ -12,12 +12,12 @@ const mdParser = new MarkdownIt({
 });
 
 // for handling underlined text
-mdParser.inline.ruler.after('emphasis', 'underline', (state, silent) => {
+mdParser.inline.ruler.after("emphasis", "underline", (state, silent) => {
   const start = state.pos;
   const marker = state.src[start];
 
   // Check if the marker is `+` and followed by another `+`
-  if (marker !== '+' || state.src[start + 1] !== '+') {
+  if (marker !== "+" || state.src[start + 1] !== "+") {
     return false;
   }
 
@@ -26,13 +26,13 @@ mdParser.inline.ruler.after('emphasis', 'underline', (state, silent) => {
 
   // Search for the closing `++`
   while (match < state.src.length) {
-    if (state.src[match] === '+' && state.src[match + 1] === '+') {
+    if (state.src[match] === "+" && state.src[match + 1] === "+") {
       // Only proceed if not in silent mode
       if (!silent) {
         // Create tokens for the underlined content
-        state.push('underline_open', 'u', 1); // Opening <u> tag
-        state.push('text', '', 0).content = state.src.slice(start + 2, match); // Underlined content
-        state.push('underline_close', 'u', -1); // Closing </u> tag
+        state.push("underline_open", "u", 1); // Opening <u> tag
+        state.push("text", "", 0).content = state.src.slice(start + 2, match); // Underlined content
+        state.push("underline_close", "u", -1); // Closing </u> tag
       }
       state.pos = match + 2; // Move the position to after the closing `++`
       return true;
@@ -53,46 +53,6 @@ const MarkdownRenderer = ({ content }) => {
     <div dangerouslySetInnerHTML={{ __html: renderedHTML }} />
   );
 };
-
-// commonmark mode
-//const md = markdownit('commonmark')
-
-// default mode
-
-// const mdParser = new MarkdownIt({
-//   html: true, // Allows HTML to be rendered
-// });
-//
-// enable everything
-// const md = markdownit({
-//   linkify: true,
-//   html: false,
-//   typographer: true,
-//   highlight: function (/*str, lang*/) {
-//     return "";
-//   },
-// });
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 class Previewer extends React.Component {
   constructor(props) {
